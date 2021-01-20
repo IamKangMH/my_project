fc9cee243a1ce0b551afd13f9b54de135d6dd677
minhe <minhe@DESKTOP-Q5UP4F5>
2021-01-20 15:10:19 +0900

    my project 첫번째 커밋

---
+++ src/org/weekTwo/HelloWorldMyHome.java
@@ -0,0 +1,12 @@
+package org.weekTwo;
+
+public class HelloWorldMyHome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String user = "KangMH";
+		System.out.println("User: " + user);
+		
+	}
+
+}
